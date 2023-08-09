7ffeb1be9cb83880b1536a8f9004e350f00db17a
Erik Demchak <demcsakerik@gmail.com>
2023-08-09 18:19:26 +0200

    add: CreateUser dto, added different examples of route handlers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/CreateUser.dto';
 import { Request, Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   @Get('/')
-  getUsers() {
+  getUsers(@Query('sortBy') sortBy: string) {
+    console.log('sortBy: ', sortBy);
     return [
       { id: 1, name: 'Erik' },
       { id: 2, name: 'Lolka' },
     ];
+  }
+
+  @Get(':id')
+  getUserById(@Param() userId: string) {
+    console.log('userId: ', userId);
+    return [{ id: 1, name: 'Erik' }];
   }
 
   @Get('posts')
@@ -19,9 +36,15 @@ export class UsersController {
     ];
   }
 
-  @Post('/create')
-  createUser(@Req() request: Request, @Res() response: Response) {
+  @Post('/createtest')
+  createUserTest(@Req() request: Request, @Res() response: Response) {
     console.log('post body: ', request.body);
     response.send('Created');
+  }
+
+  @Post('/create')
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
   }
 }
